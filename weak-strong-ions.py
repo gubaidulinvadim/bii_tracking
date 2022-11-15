53f1d79158f7c8f46b99ace72506e53a4da37d9a
gubaidulinvadim <gubaidulinvadim@protonmail.com>
2022-11-15 17:26:24 +0100

    updated slurm script

--- weak-strong-ions.py
+++ weak-strong-ions.py
@@ -84,7 +84,7 @@ def run(n_macroparticles, n_macroparticles_ions):
 
 if __name__ == "__main__":
     slurm_array_task_id = int(sys.argv[1])
-    n_macroparticles = np.array([int(1e3), int(5e3), int(1e4), int(5e4), int(1e5), int(5e5), int(1e6)])
+    n_macroparticles = np.array([int(1e3), int(5e3), int(1e4), int(5e4), int(1e5)])
     run(n_macroparticles[slurm_array_task_id], n_macroparticles_ions=int(1e4))
     sys.exit()
     
