c3ffa84ce4dae976d744959cd5d4e86a5b2e5918
gubaidulinvadim <gubaidulinvadim@protonmail.com>
2022-11-21 18:10:31 +0100

    changes to slurm scripts

--- weak-strong-ions.py
+++ weak-strong-ions.py
@@ -13,7 +13,7 @@ from PyHEADTAIL.general import pmath as pm
 from scipy.constants import m_p
 from SOLEILII_parameters.SOLEILII_CDR_parameters import *
 import numpy as np
-N_TURNS = int(1000)
+N_TURNS = int(100)
 H_RF = 416
 N_SEGMENTS = int(500)
 np.random.seed(42)
@@ -35,17 +35,21 @@ def run(n_macroparticles, n_macroparticles_ions):
         )    
     electron_bunch_list = []
     monitor_list = []
+    r_x = 2*np.sqrt(EPSILON_X*BETA_X_SMOOTH)
+    r_xp = 2*np.sqrt(EPSILON_X*BETA_X_SMOOTH)
+    r_y = 2*np.sqrt(EPSILON_Y*BETA_Y_SMOOTH)
+    r_yp = 2*np.sqrt(EPSILON_Y*BETA_Y_SMOOTH)  
     for ind, h in enumerate(range(H_RF)):
         electron_bunch = generators.ParticleGenerator(macroparticlenumber=n_macroparticles,
                                                     intensity=INTENSITY_PER_BUNCH,
                                                     charge=e, gamma=GAMMA, mass=m_p,
                                                     circumference=CIRCUMFERENCE,
-                                                    distribution_x=generators.kv2D(3*np.sqrt(EPSILON_X*BETA_X_SMOOTH), 3*np.sqrt(EPSILON_X/BETA_X_SMOOTH)),
+                                                    distribution_x=generators.kv2D(r_x, r_xp),
                                                     alpha_x=ALPHA_X_SMOOTH, beta_x=BETA_X_SMOOTH,
-                                                    distribution_y=generators.kv2D(3*np.sqrt(EPSILON_Y*BETA_Y_SMOOTH), 3*np.sqrt(EPSILON_Y/BETA_Y_SMOOTH)),
+                                                    distribution_y=generators.kv2D(r_y, r_yp),
                                                     alpha_y=ALPHA_Y_SMOOTH, beta_y=BETA_Y_SMOOTH,
                                                     distribution_z=generators.gaussian2D_asymmetrical(SIGMA_Z, SIGMA_DP),
-                                                    limit_n_rms_x=3., limit_n_rms_y=3.,
+                                                    # limit_n_rms_x=3., limit_n_rms_y=3.,
                                                     printer=SilentPrinter()
                                                     ).generate()
         electron_bunch.z += h*CIRCUMFERENCE/H_RF
@@ -73,14 +77,13 @@ def run(n_macroparticles, n_macroparticles_ions):
         beam_ion_elements.append(BeamIonElement(n_macroparticles_max = n_macroparticles_ions))
     trans_one_turn = [item for sublist in zip(trans_one_turn, beam_ion_elements) for item in sublist]
     for turn in range(N_TURNS):
-# turn = 0
         for index, m_ in enumerate(trans_one_turn):
             for bunch_index, electron_bunch in enumerate(electron_bunch_list):
                 m_.track(electron_bunch)    
                 long_map.track(electron_bunch)
-                if index % 2 ==0:
+                if index % 2 == 0:
                     monitor_list[bunch_index].dump(electron_bunch)    
-        return 0
+    return 0
 
 if __name__ == "__main__":
     slurm_array_task_id = int(sys.argv[1])
