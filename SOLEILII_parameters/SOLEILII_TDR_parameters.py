fd2909425572cf2d7ca2edd0a42dd184ee9649b3
gubaidulinvadim <gubaidulinvadim@protonmail.com>
2023-02-06 16:47:10 +0100

    added physical constants definitions and  removed some comments

--- SOLEILII_parameters/SOLEILII_TDR_parameters.py
+++ SOLEILII_parameters/SOLEILII_TDR_parameters.py
@@ -1,6 +1,9 @@
-from scipy.constants import c, e, m_e, m_p, epsilon_0, pi
+from scipy.constants import physical_constants, c, e, m_e, m_p, epsilon_0, pi, N_A, R
 from numpy import sqrt
 
+r_e = physical_constants["classical electron radius"][0]
+m_u = physical_constants["unified atomic mass unit"][0]
+r_u = 1/(4*pi*epsilon_0)*e**2/(m_u*c**2)
 
 CIRCUMFERENCE = 353.74
 R = CIRCUMFERENCE/(2*pi)
@@ -24,11 +27,12 @@ TAU_X = 7.3e-3
 TAU_Y = 13.1e-3
 TAU_Z = 11.7e-3
 Q_X = 54.2
-Q_Y = 18.31
+Q_Y = 18.3
 Qp_X = 1.6
 Qp_Y = 1.6
-BETA_X_SMOOTH = 5.58 # R/Q_X
-BETA_Y_SMOOTH = 3.92 # R/Q_Y
+PRESSURE = 1e-7
+BETA_X_SMOOTH = 5.58  
+BETA_Y_SMOOTH = 3.92 
 ALPHA_X_SMOOTH = 0
 ALPHA_Y_SMOOTH = 0
 I = 500e-3
